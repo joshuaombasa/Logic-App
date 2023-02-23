b5905d326ecd31b7b407a835b3f39212796fdad4
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-02-23 22:40:11 +0300

    updated script

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { catsData } from '/data.js'
 
 const displayEmotions = document.getElementById('display-emotions')
+const getImgBtn = document.getElementById("get-img-btn")
+const gifsOnlyOption = document.getElementById("gifs-only-option")
 
 function getEmotionsArray(cats) {
     let emotionsArray = []
@@ -23,12 +25,33 @@ function renderEmotionsRadios(cats) {
         
         html += `<div class="feeling-styling">
                     <label for="${feeling}">${feeling}</label>
-                    <input type="radio" id="${feeling}" value="${feeling}" name="${feeling}">
+                    <input type="radio" id="${feeling}" value="${feeling}" name="emotions">
                  </div>`
     }
 
     displayEmotions.innerHTML = html
 }
 
-console.log(renderEmotionsRadios(catsData))
+renderEmotionsRadios(catsData)
 
+displayEmotions.addEventListener("change", showCheckedOption)
+
+function showCheckedOption(e) {
+    let radioElArray = document.getElementsByClassName("feeling-styling")
+    for (let radioEl of radioElArray) {
+        radioEl.classList.remove("highlight")
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+getImgBtn.addEventListener("click",getMatchingCatsArray)
+
+function getMatchingCatsArray() {
+    const isGif = gifsOnlyOption.checked
+    console.log(isGif)
+    if (document.querySelector('input[type="radio"]:checked')) {
+        const checkedRadio = document.querySelector('input[type="radio"]:checked').value
+        console.log(checkedRadio)
+    }
+
+}
